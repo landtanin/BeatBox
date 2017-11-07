7c1cadaaba0f32d66bcb686c5c9d6c57a1a545be
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-07 16:21:08 +0000

    Observable data

--- app/src/main/java/com/landtanin/beatbox/SoundViewModel.java
+++ app/src/main/java/com/landtanin/beatbox/SoundViewModel.java
@@ -1,10 +1,13 @@
 package com.landtanin.beatbox;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by Tanin on 07/11/2017.
  */
 
-public class SoundViewModel {
+public class SoundViewModel extends BaseObservable{
 
     private Sound mSound;
     private BeatBox mBeatBox;
@@ -21,6 +24,7 @@ public class SoundViewModel {
 
     }
 
+    @Bindable
     public String getTitle() {
 
         return mSound.getmName();
@@ -31,7 +35,10 @@ public class SoundViewModel {
         return mSound;
     }
 
-    public void setmSound(Sound mSound) {
-        this.mSound = mSound;
+    public void setmSound(Sound sound) {
+        mSound = sound;
+
+        // notifies your binding class that all of the Bindable fields on your objects have been updated
+        notifyChange();
     }
 }
